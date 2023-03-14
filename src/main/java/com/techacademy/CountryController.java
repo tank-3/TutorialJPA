99eb7cc79b3e5377fa0ea2b393ad2d1bb09ce82e
tank-3 <taku3.na3.1415@gmail.com>
2023-03-14 12:21:05 +0900

    detail,update,delete追加

---
+++ src/main/java/com/techacademy/CountryController.java
@@ -0,0 +1,45 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("country")
+public class CountryController {
+    private final CountryService service;
+
+    public CountryController(CountryService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public String getList(Model model) {
+        model.addAttribute("countrylist", service.getCountryList());
+        return "country/list";
+    }
+    @GetMapping(value = {"/detail", "/detail/{code}/"})
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        Country country = code != null ? service.getCountry(code) : new Country();
+        model.addAttribute("country", country);
+        return "country/detail";
+    }
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name, @RequestParam("population") int population, Model model) {
+        service.updateCountry(code, name, population);
+        return "redirect:/country/list";
+    }
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        return "country/delete";
+    }
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        service.deleteCountry(code);
+        return "redirect:/country/list";
+    }
+}
---
+++ src/main/java/com/techacademy/CountryService.java
@@ -0,0 +1,36 @@
+package com.techacademy;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CountryService {
+    private final CountryRepository repository;
+
+    public CountryService(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Country> getCountryList() {
+        return repository.findAll();
+    }
+    
+    public Country getCountry(String code) {
+        Optional<Country> option = repository.findById(code);
+        Country country = option.orElse(null);
+        return country;
+    }
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+    
+}
